0b602bbf5596fd0d18344cc36d8019d6d208d6f0
lvbart <lvnan.airchina@gmail.com>
2020-02-06 09:27:34 +0800

    add tab control

---
+++ src/components/common/tabControl/TabControl.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tab-control">
+    <div v-bind:key="item.id" v-for="item in titles"
+    class="tab-control-item" :class="{active: index === currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(item) {
+      this.currentIndex = index;
+    }
+  }
+};
+
+</script>
+
+<style>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    position: sticky;
+    top: 100px;
+  }
+
+  .tab-control-item {
+    flex: 1;
+  }
+
+  .active {
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-tint)
+  }
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,34 +1,22 @@
 <template>
   <div id="home">
-    <div class="bg-gray-200 min-h-screen p-16 flex justify-center">
-      <card :property="property"/>
-      <card class="py-12" :property="property"/>
 
-    </div>
+      <tab-control :titles="['trend', 'news', 'select']"/>
+
   </div>
 
 </template>
 
 <script>
-import Card from '../../components/common/card/Card.vue'
+
+import TabControl from '../../components/common/tabControl/TabControl.vue'
 
 export default {
   name: 'Home',
   components: {
-    Card,
+    TabControl,
   },
-  data: () => ({
-    property: {
 
-      beds: 3,
-      baths: 2,
-      title: "Modern home in city center",
-      priceInCents: 190000,
-      formattedPrice: "$1,900.00",
-      reviewCount: 34,
-      rating: 4,
-    }
-  })
 };
 </script>
 
