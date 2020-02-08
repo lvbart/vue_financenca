9d5c52486346ec49978b65d01a68ec36c89efc6c
lvbart <lvnan.airchina@gmail.com>
2020-02-08 15:26:07 +0800

    change sort

--- src/views/sort/Sort.vue
+++ src/views/sort/Sort.vue
@@ -1,26 +1,17 @@
 <template>
-  <card class="py-12" :property="property"/>
+
 </template>
 
 <script>
-import Card from '../../components/common/card/Card.vue'
+
 
 export default {
   name: 'Sort',
   components: {
-    Card,
+
   },
   data: () => ({
-    property: {
 
-      beds: 3,
-      baths: 2,
-      title: "Modern home in city center",
-      priceInCents: 190000,
-      formattedPrice: "$1,900.00",
-      reviewCount: 34,
-      rating: 4,
-    }
   })
 };
 </script>
