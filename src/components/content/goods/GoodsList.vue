f18a2854054bd941b6c96ba0192b3c862c43155e
NAN LU <lvnan.airchina@gmail.com>
2020-02-19 15:03:24 +0800

    Add files via upload
    update goodsList, to show item detail.

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bg-indigo-200 grid grid-cols-3">
+    <div :key="index" v-for="(item, index) in goodsList">
+      <div class="">
+        <img :src="item.show.img" alt="">
+      </div>
+
+      <div class="mt-4 text-center">
+        <p class="truncate">{{item.title}}</p>
+        <div class="flex">
+          <span class="flex-1 text-xl text-black">¥{{item.price}}</span>
+          <span class="flex-1 text-normal">{{item.cfav}}</span>
+        </div>
+
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'GoodsList',
+  props: {
+    goodsList: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.push('/detail' + this.goods.new.iid)
+
+    },
+  }
+};
+
+</script>
+
+<style>
+
+
+</style>
