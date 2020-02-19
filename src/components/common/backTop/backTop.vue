4f130e34c4b8504e56d756793ff018492d6a8218
NAN LU <lvnan.airchina@gmail.com>
2020-02-19 15:12:11 +0800

    Add files via upload
    add top

---
+++ src/components/common/backTop/backTop.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-12 absolute bottom-0 right-0">
+    <img src="../../../assets/img/arrow-up-circle-outline.svg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'backTop',
+
+}
+</script>
+
+<style>
+
+</style>
