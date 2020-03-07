4e3475b20b0a0ba2212516e62ca8967a95df164a
NAN LU <lvnan.airchina@gmail.com>
2020-03-07 10:43:06 +0800

    Add files via upload
    add new shop info: no need to create a class

---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+
+    <nav-bar class="nav-bar">
+      <div slot="left" class="h-16 w-16 px-2 py-1 ml-3" @click="backClick">
+        <div>
+          <img src="../../../../src/assets/img/arrow-back-outline.svg" alt="">
+        </div>
+
+
+      </div>
+      <div slot="center" class="grid grid-cols-4 text-center text-black text-xl">
+        <div v-for="(item, index) in titles"
+        :class="{active: index === currentIndex}"
+        @click="titleClick(index)"
+        v-bind:key="item">
+          {{item}}
+        </div>
+      </div>
+      </nav-bar>
+  </div>
+
+</template>
+
+<script>
+import NavBar from '../../../components/common/navbar/NavBar.vue'
+
+
+export default {
+  name: 'DetailNavBar',
+  components: {
+    NavBar,
+
+  },
+  data() {
+    return {
+      titles: ['product', 'params', 'comments', 'recommend'],
+      currentIndex: 0,
+
+    }
+  },
+  methods: {
+    titleClick(index) {
+      this.currentIndex = index;
+      this.$emit("titleClick", index);
+    },
+    backClick() {
+      this.$router.back()
+    },
+  }
+
+
+}
+</script>
+
+<style scoped>
+.active {
+  color: red,
+}
+
+.nav-bar {
+  position: relative;
+  z-index: 1;
+}
+
+</style>
---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+<swiper class="detail-swiper">
+  <swiper-item class="item" v-bind:key="index" v-for="(item, index) in images">
+    <img :src="item" alt="">
+  </swiper-item>
+</swiper>
+</div>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from 'components/common/swiper'
+export default {
+  name: 'DetailSwiper',
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+  props: {
+    images: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  .item {
+    text-align: center;
+    height: 500px;
+  }
+
+</style>
